7e76b7111eb1350ddbcc0693461a40d437d17356
huggo <hutricot@student.42.fr>
2020-02-03 18:41:35 +0100

    kk

--- js/index.js
+++ js/index.js
@@ -5,6 +5,18 @@ var cook = require('cookie-parser')
 const app = express();
 app.use(cook()) 
 
+function parseCookies (request) {
+    var list = {},
+        rc = request.headers.cookie;
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+}
+
 app.get('/cook/:add', (req, res) =>{
 	res.cookie(req.params.add, "liste").send('cookie set');
 });
@@ -17,6 +29,11 @@ app.get('/:coucou', (req, res) => {
 	var noms = ['un', 'deux','trois']
 	res.render('vue.ejs', {test: req.params.coucou, noms: noms});
 });
+app.get('/', (req, res) =>{
+	var cookies = parseCookies(request);
+	res.send(cookies);
+});
+
  
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
